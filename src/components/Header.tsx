20e313472ea4c9975ed2b2253f4939001bae46e0
Patrick Coston <patcoston@gmail.com>
2022-12-04 20:52:04 -0500

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import { Typography, Box, useTheme } from '@mui/material'
+import { tokens } from '../theme'
+
+interface HeaderProps {
+  title: string
+  subtitle: string
+}
+
+const Header: FC<HeaderProps> = ({ title, subtitle }) => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: '5px' }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  )
+}
+
+export default Header
