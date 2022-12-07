daf9424c8d3d8e325740c0d2fac28b59c4f2b8c3
Patrick Coston <patcoston@gmail.com>
2022-12-06 19:45:22 -0500

    Added Invoices

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Dashboard from './scenes/global/Dashboard'
 import { Route, Routes } from 'react-router-dom'
 import Team from './scenes/team'
 import Contacts from './scenes/contacts'
-// import Invoices from './scenes/invoices'
+import Invoices from './scenes/invoices'
 // import Bar from './scenes/bar'
 // import Form from './scenes/form'
 // import Line from './scenes/line'
@@ -31,7 +31,7 @@ const App: FC = () => {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
